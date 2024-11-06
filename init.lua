f42070da3e4d8df2f60d46bbac2bb2ff915dc88c
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-11-06 12:12:58 +0000

    Added keymap for line diagnostics

--- init.lua
+++ init.lua
@@ -107,6 +107,7 @@ vim.diagnostic.config {
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Open line diagnostics' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
