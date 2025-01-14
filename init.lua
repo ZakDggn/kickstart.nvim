a09dfd82dcc88ea18e54af20f2fc8aabd6a86c96
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2025-01-14 19:01:26 +0000

    Remove tokyonight theme

--- init.lua
+++ init.lua
@@ -771,15 +771,12 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
     end,
   },
-  'folke/tokyonight.nvim',
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
